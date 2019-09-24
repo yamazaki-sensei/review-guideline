915bc2726d00aaba56f8ec65dbddedd1c3390348
Hiraku Ohno <ohno@toreta.in>
2019-09-24 23:40:14 +0900

    Add scripts

---
+++ env.sh
@@ -0,0 +1,2 @@
+export EN_URL="https://developer.apple.com/app-store/review/guidelines/",
+export JA_URL="https://developer.apple.com/jp/app-store/review/guidelines/"
---
+++ fetch_and_commit.sh
@@ -0,0 +1,4 @@
+source env.sh
+
+curl $EN_URL > results/en.html
+curl $JA_URL > results/ja.html
