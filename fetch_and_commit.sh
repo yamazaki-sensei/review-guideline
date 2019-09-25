e81a8979f6be35229e16dae510b92eea86f14054
hira <ohno@toreta.in>
2019-09-25 23:44:21 +0900

    Updated at 2019年 9月25日 水曜日 23時44分21秒 JST

--- fetch_and_commit.sh
+++ fetch_and_commit.sh
@@ -1,8 +1,13 @@
+set -eux
+
 curl  https://developer.apple.com/app-store/review/guidelines/ > results/en.html
 curl  https://developer.apple.com/jp/app-store/review/guidelines/ > results/ja.html
 
 git config --global user.email ohno@toreta.in
 git config --global user.name hira
 
+git checkout master
 git add .
 git commit -m "Updated at `date`"
+
+git push -u origin `git rev-parse --abbrev-ref HEAD`
