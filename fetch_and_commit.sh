e83b5a3ae9bc9ead5abb18a293274cc457f9c433
hira <ohno@toreta.in>
2019-09-26 00:07:15 +0900

    Update scripts

--- fetch_and_commit.sh
+++ fetch_and_commit.sh
@@ -10,4 +10,5 @@ git checkout master
 git add .
 git commit -m "Updated at `date`"
 
-git push -u origin `git rev-parse --abbrev-ref HEAD`
+git remote add origin2 "https://yamazaki-sensei:${GITHUB_ACTION_TOKEN}@github.com/yamazaki-sensei/review-guideline
+git push -u origin2 `git rev-parse --abbrev-ref HEAD`
