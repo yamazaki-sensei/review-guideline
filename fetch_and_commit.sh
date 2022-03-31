dfa31e17d67c51b04caecabbd7f452801661a51b
hira <ohno@thelooper.jp>
2022-04-01 08:00:37 +0900

    Update fetch_and_commit.sh

--- fetch_and_commit.sh
+++ fetch_and_commit.sh
@@ -5,7 +5,7 @@ git checkout master
 curl  https://developer.apple.com/app-store/review/guidelines/ > results/en.html
 curl  https://developer.apple.com/jp/app-store/review/guidelines/ > results/ja.html
 
-git config --global user.email ohno@toreta.in
+git config --global user.email almichest.free@gmail.com
 git config --global user.name hira
 
 git add .
@@ -18,11 +18,3 @@ fi
 
 git remote add origin2 "https://yamazaki-sensei:${GITHUB_TOKEN}@github.com/yamazaki-sensei/review-guideline"
 git push -u origin2 `git rev-parse --abbrev-ref HEAD`
-
-curl -X POST \
-  --data-urlencode 'payload={
-    "channel": "#dev-ios",
-    "username": "Review Guideline",
-    "text": "Review Guideline has been updated. https://github.com/yamazaki-sensei/review-guideline/commits/master"
-  }' \
-${SLACK_WEBHOOK_URL}
